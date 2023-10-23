069c426296df4e46df2353d7c2e9efbb2994f845
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-10-22 17:51:33 -0700

    run npm test to test the wordSearch function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,42 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return error if the array(s) are empty", function() {
+    const result = wordSearch([], "HOWDY")
+
+    assert.isFalse(result)
+  });
+
+  it("should operate as normal if inputed 'word' is all or contains lowercase characters", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'seInfELd')
+
+    assert.isTrue(result);
+  });
+
+  it("should throw error if inputed 'word' is not a string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 1000)
+
+    assert.isFalse(result);
+  })
 });
